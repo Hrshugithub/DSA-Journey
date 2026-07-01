5ede2014d9c1afe72a5c95966d0f3910c6e7c50b
Hrshugithub <harshitrajput.hk@gmail.com>
2026-07-01 15:05:10 +0530

    To check whether the given array is sorted or not in non-decreasing order

---
+++ src/Arrays/IsSorted.java
@@ -0,0 +1,46 @@
+//## Problem Statement
+//
+//Given an `ArrayList<Integer>`, determine whether the elements are sorted in non-decreasing (ascending) order.
+//
+//Return:
+//- `true` if the ArrayList is sorted.
+//- `false` otherwise.
+//
+//### Example 1
+//
+//Input:
+//```java
+//[1, 2, 3, 4, 5]
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+
+        Main obj = new Main();
+
+        boolean result = obj.isSorted(nums);
+
+        System.out.println("Is the ArrayList sorted? " + result);
+    }
+
+    public boolean isSorted(ArrayList<Integer> nums) {
+        return check(nums);
+    }
+
+    boolean check(ArrayList<Integer> arr) {
+        for (int i = 0; i < arr.size() - 1; i++) {
+            if (arr.get(i) > arr.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
